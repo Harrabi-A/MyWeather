8f2c496b43a093d1e32f627fc0db6c3715c58e10
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-08-11 19:41:54 +0200

    Create ViewController.h

---
+++ View Controllers/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MyWeather
+//
+//  Created by Ahmed Harrabi on 10/08/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UINavigationBar *navBar ;
+
+@end
