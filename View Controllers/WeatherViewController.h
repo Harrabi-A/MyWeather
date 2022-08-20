43f76b26ca4a71d23c826842c9d4640fccb44c64
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-08-20 23:46:51 +0200

    Update WeatherViewController.h

--- View Controllers/WeatherViewController.h
+++ View Controllers/WeatherViewController.h
@@ -9,6 +9,7 @@
 #import "CityDataSource.h"
 
 @interface WeatherViewController : UIViewController
-
+@property (nonatomic, strong) City *citySearched;
+@property (nonatomic,strong) City *city;
 @property (nonatomic, strong) id<CityDataSource> dataSource;
 @end
