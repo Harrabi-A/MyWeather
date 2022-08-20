fb70a11c29ecd498f3d58f468c5c52dcea120a78
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-08-21 01:00:24 +0200

    Update MapFavoriteViewController.m

--- View Controllers/MapFavoriteViewController.m
+++ View Controllers/MapFavoriteViewController.m
@@ -9,6 +9,8 @@
 #import <MapKit/MapKit.h>
 #import "User.h"
 #import "Poi.h"
+#import "City.h"
+#import "WeatherViewController.h"
 
 @interface MapFavoriteViewController ()<MKMapViewDelegate>
 
@@ -27,10 +29,60 @@
     Poi *userPoi = [User sharedUser].city.position;
     self.favoriteList = [User sharedUser].favoriteList;
     [self centerMapToLocation:CLLocationCoordinate2DMake(userPoi.latitude, userPoi.longitude)
-                         zoom:0.1];
+                         zoom:25];
+    
+    
     //self.mapView.showsUserLocation = YES;
 }
 
+- (void)viewWillAppear:(BOOL)animated{
+    for (int i=0; i<[self.favoriteList size]; i++) {
+        City *c = [self.favoriteList getAtIndex:i];
+        [self.mapView addAnnotation:c];
+    }
+}
+
+- (MKAnnotationView *) mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    static NSString *AnnotationIdentifer = @"MapAnnotationView";
+    MKAnnotationView *view = [mapView dequeueReusableAnnotationViewWithIdentifier:AnnotationIdentifer];
+    if(!view){
+        view = [[MKPinAnnotationView alloc] initWithAnnotation:annotation
+                                               reuseIdentifier:AnnotationIdentifer];
+        view.canShowCallout = YES;
+    }
+    view.annotation = annotation;
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 36, 36)];
+    //imageView.image = [UIImage systemImageNamed:@"sun.max.fill"];
+    view.leftCalloutAccessoryView = imageView;
+    view.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    return view;
+}
+
+-(void) mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view{
+    if([view.leftCalloutAccessoryView isKindOfClass:[UIImageView class]]){
+        UIImageView *imageView = (UIImageView *)view.leftCalloutAccessoryView;
+        id<MKAnnotation> annotation = view.annotation;
+        if([annotation isKindOfClass:[City class]]){
+            City *c = (City *)annotation;
+            NSString *w = [c.weatherList getAtIndex:0].condition;
+            if ([ w isEqualToString:@"Sunny"]) {
+                imageView.image = [UIImage systemImageNamed:@"sun.max.fill"];
+            }else if ([ w isEqualToString:@"Rainy"]) {
+                imageView.image = [UIImage systemImageNamed:@"cloud.rain.fill"];
+            }else if ([ w isEqualToString:@"Cloudy"]){
+                imageView.image = [UIImage systemImageNamed:@"cloud.fill"];
+            }else if ([ w isEqualToString:@"sunAndCloud"]){
+                imageView.image = [UIImage systemImageNamed:@"cloud.sun.fill"];
+            }
+        }
+    }
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control{
+    if([control isEqual:view.rightCalloutAccessoryView]){
+        [self performSegueWithIdentifier:@"ShowWeather" sender:view];
+    }
+}
 
 - (void) centerMapToLocation:(CLLocationCoordinate2D)location
                         zoom:(double)zoom{
@@ -41,14 +93,27 @@
     [self.mapView setRegion:mapRegion];
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"ShowWeather"]) {
+        if ([segue.destinationViewController isKindOfClass:[WeatherViewController class]]) {
+            WeatherViewController *vc = (WeatherViewController *)segue.destinationViewController;
+            if ([sender isKindOfClass:[MKAnnotationView class]]) {
+                MKAnnotationView *view = (MKAnnotationView *)sender;
+                id<MKAnnotation> annotation = view.annotation;
+                if([annotation isKindOfClass:[City class]]){
+                    City *c = (City *)annotation;
+                    vc.citySearched = c;
+                }
+            }
+        }
+    }
 }
-*/
+
 
 @end
